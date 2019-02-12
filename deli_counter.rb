00a9ece230d84df84f4f15da1ba6b1c09393a48a
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-12 17:24:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,6 @@
-# Write your code here.
 katz_deli = []
+count = 0
+#The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rache
 def line(arr)
   str = "The line is currently: "
   if(arr.length == 0)
@@ -29,6 +30,23 @@ def now_serving(arr)
     puts "Currently serving #{arr[0]}."
     arr.shift
   end
-    
-    
 end
+
+def take_a_number2(arr, name)
+  name = name.split
+  name.each do |a| 
+    arr << a
+    count += 1
+    puts "Welcome, #{a}. You are number #{count} in line."
+  end
+end
+
+
+
+
+
+
+
+
+
+
